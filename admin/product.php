3fed322298d9927b2832734594fc24ce3c3fa877
Quinn <qgbritt@gmail.com>
2020-02-18 18:00:04 -0500

    Update admin

---
+++ admin/product.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+
+<?php
+$title = 'Welcome to ECom';
+require_once '../common/head.php'
+?>
+
+<body>
+  <?php
+  require_once '../common/script.php'
+  ?>
+</body>
+
+</html>
