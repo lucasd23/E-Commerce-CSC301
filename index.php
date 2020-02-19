f5350de44fa9bc07a6e02c0d97f66a3c6ef946b0
Quinn <qgbritt@gmail.com>
2020-02-19 17:10:12 -0500

    Fix issue added in commit 82419892837b64ae1f05d859cd5be6d4d18fd1d1

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php // ! Where main PHP processing is supposed to happen
+
+require_once './utils/json-utils.php';
+$roadBikes = readJSON('./data/items/road.json');
+$mountainBikes = readJSON('./data/items/mountain.json');
+$hybridBikes = readJSON('./data/items/hybrid.json');
+$kidBikes = readJSON('./data/items/kid.json');
+// Use array_merge(array1 array2...) to add arrays together if necessary
+
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -7,72 +18,8 @@ require_once './common/head.php';
 ?>
 
 <body class="text-center">
-  <!doctype html>
-  <html lang="en">
 
   <?php
-  $title = 'Welcome to ECom';
-  require_once './common/head.php';
-  require_once './utils/json-utils.php';
-  $roadBikes = readJSON('./data/items/road.json');
-  //echo $items;
-  ?>
-
-  <body class="text-center">
-    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-      <header class="masthead mb-auto">
-        <div class="inner">
-          <h3 class="masthead-brand">Items for Sale</h3>
-          <nav class="nav nav-masthead justify-content-center">
-            <a class="nav-link active" href="index.php">Home</a>
-          </nav>
-        </div>
-      </header>
-
-      <main role="main" class="inner cover">
-        <div class="card text-center">
-          <div class="card-header">
-            Product Categories
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">Category 1</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-          <div class="card-header">
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">Category 2</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-          <div class="card-header">
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">Category 3</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-          <div class="card-footer text-muted">
-            2 days ago
-          </div>
-        </div>
-      </main>
-
-      <footer class="mastfoot mt-auto">
-        <div class="inner">
-          <p>Theme for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://bootswatch.com/">Bootswatch</a>.</p>
-        </div>
-      </footer>
-    </div>
-
-    <?php
-    require_once 'script.php'
-    ?>
-  </body>
-
-  </html>
-
   require './common/nav.php'
   ?>
 
@@ -119,6 +66,7 @@ require_once './common/head.php';
   <?php
   require_once './common/script.php'
   ?>
+
 </body>
 
 </html>
