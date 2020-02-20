005a3486619c9f8fbd01804b86db3ab47387b623
Dillon Lucas <dilloncarterlucas@gmail.com>
2020-02-20 17:55:06 -0500

    On branch master  Changes to be committed: 	modified:   createProduct.php

--- createProduct.php
+++ createProduct.php
@@ -1,8 +1,8 @@
 <?php
-$file = 'data/items/' . $_GET['category'];
+$file = 'data/items/' . $_GET['category'].'.json';
 if (!isset($_SERVER['CONTENT_LENGTH'])) {
 } else {
-    require_once('JSONtility.php');
+    require_once('utils/json-utils.php');
     writeJSON($file, $_POST);
 }
 
@@ -20,11 +20,11 @@ require './common/head.php'
     ?>
     <main>
         <h1>Create Bike</h1>
-        <form action="create.php" method="POST">
+        <form action="createProduct.php?category=<?=$_GET['category']?>" method="POST" enctype="multipart/form-data">
             Name: <input name="name" type="text"><br>
             Price: $<input name="price" type="number" step="0.01"><br>
             Wheel Size: <input name="wheelSize" type="number" step="0.1"><br>
-            Color: $<input name="color" type="number" step="0.01"><br>
+            Color: <input name="color" type="text"><br>
             Link to Photo: <input name="picture" type="text"><br>
             <button type="submit">Submit</button>
         </form>
