27847848b2438d8a65a050180bf4a83850379a9d
Dillon Lucas <dilloncarterlucas@gmail.com>
2020-02-20 16:52:40 -0500

    On branch master  Changes to be committed: 	modified:   createProduct.php

--- createProduct.php
+++ createProduct.php
@@ -10,8 +10,8 @@ else {
 <html>
 <h1>Create Bike</h1>
 <form action="create.php" method="POST">
-    name: <input name="name" type="text"> Last: <input name="last" type="text"><br>
-    Link to Photo: <input name="picture" type="text"><br>
+    Name: <input name="name" type="text"><br>
+    Price: <input name="price" type="number" step="0.01"><br>
     Apartment Number: <input name="aptNum" type="text"><br>
     Rent: $<input name="rent" type="number" step="0.01"><br>
     Number of Late Payments: <input name="latePayments" type="number"><br>
